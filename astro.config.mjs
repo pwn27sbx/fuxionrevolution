8a300a3419134b5232a6d1ce45853d0404851d4f
pwn27sbx <pwn27sbx@gmail.com>
2026-06-09 14:11:10 -0500

    changes responsive add new footer change layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
